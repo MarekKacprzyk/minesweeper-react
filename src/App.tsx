3713009ce5f0705cc315105cad20b6a5bdf8e67c
Marek Kacprzyk <kacprzyk.marek@Gmail.com>
2026-05-21 00:20:07 +0200

    feat: add routing and page placeholders
    BrowserRouter z trasami /, /game, /scores.
    Placeholder komponenty StartPage, GamePage, ScoresPage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import StartPage from './pages/StartPage';
+import GamePage from './pages/GamePage';
+import ScoresPage from './pages/ScoresPage';
+
 function App() {
-  return <></>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/scores" element={<ScoresPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
